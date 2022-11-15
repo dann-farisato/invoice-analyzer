90273eb14df495430829c47883efd29076b9664c
Dan Farisato <danfarisato@Dans-MBP.lan>
2022-11-15 17:10:33 +0100

    drop - style done / extra info done

--- src/app/components/dropzone/dropzone.component.ts
+++ src/app/components/dropzone/dropzone.component.ts
@@ -12,9 +12,6 @@ import { detectProducts } from 'src/utils/detectProducts';
 export class DropzoneComponent implements OnInit {
   result!: any;
   file: any;
-  shortLink: string = "";
-  loading: boolean = false; // Flag variable
-  percentage!: Observable<number>;
   products: string[] = [];
   prodResult: string[] = [];
   progressBar!: number;
@@ -25,12 +22,8 @@ export class DropzoneComponent implements OnInit {
   }
   onChange(event: any) {
     this.file = event.target.files[0];
-    console.log('file', this.file)
   }
   onUpload() {
-    // let formData = new FormData();
-    // formData.set('file', this.file);
-    // this.http.post('http://localhost:3007/upload', formData).subscribe((response) => { console.log(response) });
     Tesseract
       .recognize(this.file)
       .progress((data) => this.progressBar = data.progress)
@@ -38,11 +31,8 @@ export class DropzoneComponent implements OnInit {
         console.log(res.text);
         this.result = res.text;
         this.products = detectProducts(res.text)
-        console.log('products : ', this.products)
         this.productService.postProducts(this.products).subscribe(data => {
-          console.log(this.prodResult);
           this.prodResult = data;
-
         });
       })
       .catch(console.error);
